995e47c45a186023c0711acc6ee302052d40bf31
hualeel <hualeel@sina.com>
2020-02-20 21:35:57 +0800

    增加api列表配置项

--- src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_provider/TsfSpringCloudProviderApplication.java
+++ src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_provider/TsfSpringCloudProviderApplication.java
@@ -1,5 +1,6 @@
 package com.hua.tsf_spring_cloud_demo.tsf_spring_cloud_provider;
 
+import com.tencent.tsf.monitor.annotation.EnableTsfMonitor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableTsf
+//服务监控
+@EnableTsfMonitor
 public class TsfSpringCloudProviderApplication {
 
     public static void main(String[] args) {
