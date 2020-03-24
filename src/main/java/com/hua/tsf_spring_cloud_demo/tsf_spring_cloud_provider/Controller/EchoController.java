1dbb6a05b887b9080da19858150c499fab20f886
hualeel <hualeel@sina.com>
2020-03-24 16:12:36 +0800

    修改自定义鉴权规则和日志输出

--- src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_provider/Controller/EchoController.java
+++ src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_provider/Controller/EchoController.java
@@ -31,28 +31,27 @@ public class EchoController {
     // notes 对应 API 描述
     @ApiOperation(value = "/echo/{param}", notes = "provide微服务回显字符串", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping(value = "/echo/{param}", method = RequestMethod.GET)
-    public String echo(@PathVariable String param,@RequestParam(required = false) String tagName,
-                       @RequestParam(required = false) String tagValue,
-                       @RequestParam(required = false) String consumer_config_value,
-                       @RequestParam(required = false) String global_config_value) {
+    public String echo(@PathVariable String param,
+                       @RequestParam(required = false) String user) {
         // 通过url传参设置自定义标签
-        if (!StringUtils.isEmpty(tagName)) {
-            TsfContext.putTag(tagName, tagValue);
-            TsfContext.putCustomMetadata(new CustomMetadata(tagName,tagValue));
+        if (!StringUtils.isEmpty(user)) {
+            TsfContext.putTag("user", user);
+            TsfContext.putCustomMetadata(new CustomMetadata("user", user));
         }
 
 //         日志
         LOG.info("tsf-provider-demo -- request param: [" + param + "]");
 
 //        取得application.yml中的值
-        String result = "Provider Version: 2.0" + "<br>"
+        String result = "<h2>Provider Version: 1.0<h2>" + "<br>"
                 + "Request param:" + param + "<br>"
                 + "Provider.config.name:" + providerNameConfig.getName() + "<br>"
-                + "Global.config.name:" + globalConfig.getName();
+                + "Global.config.name:" + globalConfig.getName() + "<br>";
 
         LOG.info("tsf-provider -- provider config name: [" + providerNameConfig.getName() + ']');
         LOG.info("tsf-provider -- global config name: [" + globalConfig.getName() + ']');
         LOG.info("tsf-provider-demo -- response info: [" + result + "]");
+        LOG.info("tsf-provider-customize tag : [" + "user=" + user + "]");
 
         return result;
     }
